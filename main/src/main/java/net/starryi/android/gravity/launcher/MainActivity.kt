a67d8d15f2e6d8a31d351e6ebd84505f4516576b
Starryi <starryi@foxmail.com>
2020-05-13 18:57:59 +0800

    init repository

---
+++ main/src/main/java/net/starryi/android/gravity/launcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.starryi.android.gravity.launcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
